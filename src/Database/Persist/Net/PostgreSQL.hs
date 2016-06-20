c9185042243bf69e6f57e0573a63be956e6e1b34
James Johnson II <james.b.johnsonii@gmail.com>
2016-06-20 16:31:40 -0400

    update PostgreSQL.hs

--- src/Database/Persist/Net/PostgreSQL.hs
+++ src/Database/Persist/Net/PostgreSQL.hs
@@ -2,11 +2,11 @@
 --   from the @ip@ package. These instances only work for
 --   PostgresSQL. The following PostgreSQL column types are
 --   used for each data types:
---   
+--
 --   * 'IPv4': @inet@
 --   * 'Mac': @macaddr@
---   
-module Database.Persist.Net.PostgreSQL 
+--
+module Database.Persist.Net.PostgreSQL
   () where
 
 import Database.Persist
@@ -29,7 +29,7 @@ instance PersistField IPv4 where
     PersistDbSpecific s -> case IPv4ByteString.decode s of
       Just x -> Right x
       Nothing -> Left (Text.pack "PersistValue IPv4: Invalid format")
-    _ -> Left (Text.pack "PersistValue IPv4: Not a PersistDbSpecific")
+    y -> Left $ Text.pack "PersistValue IPv4: Not a PersistDbSpecific" <> Text.pack (show y)
 
 instance PersistFieldSql IPv4 where
   sqlType _ = SqlOther (Text.pack "inet")
@@ -40,7 +40,7 @@ instance PersistField Mac where
     PersistDbSpecific s -> case MacByteString.decode s of
       Just x -> Right x
       Nothing -> Left (Text.pack "PersistValue MAC: Invalid format")
-    _ -> Left (Text.pack "PersistValue MAC: Not a PersistDbSpecific")
+    y -> Left $ Text.pack "PersistValue MAC: Not a PersistDbSpecific" <> Text.pack (show y)
 
 instance PersistFieldSql Mac where
   sqlType _ = SqlOther (Text.pack "macaddr")
